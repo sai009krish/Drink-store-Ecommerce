a2149ac141778828dd448dbb2321d52c1895d332
sai krishna <sanamnvskrishna@gmail.com>
2021-05-26 14:01:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let cart = []
+const cartDOM = document.querySelector('.cart')
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]')
+addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+    addToCartButtonDOM.addEventListener('click', () => {
+        const productDOM = addToCartButtonDOM.parentNode
+        const product = {
+            image: productDOM.querySelector('.product__image').getAttribute('src'),
+            name: productDOM.querySelector('.product__name').innerText,
+            price: productDOM.querySelector('.product__price').innerText
+        }
+
+        const isInCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0)
+
+        if(!isInCart) {
+            cartDOM.insertAdjacentHTML('beforeend', `
+                <div class="cart__item">
+                    <img class="cart__item__image" src="${product.image}" alt="${product.name}"/>
+                    <h3 class="cart__item__name">${product.name}</h3>
+                    <h3 class="cart__item__price">${product.price}</h3>
+                </div>
+            `)
+            cart.push(product)
+            addToCartButtonDOM.innerText = 'In Cart'
+        }
+    })
+})
